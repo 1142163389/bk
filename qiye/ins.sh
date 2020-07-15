e9d645da8517c23a3ad24d096282f03bcbef0dbd
liuyu <yetlau@163.com>
2020-07-15 14:50:04 +0800

    add qiye

---
+++ qiye/ins.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+data='/data/install'
+num="10.0.0"
+ping2 () {
+    while :   
+    do
+      read -p "请输入中控机ip:"      ip2
+      ping  -c 2  -w 2 $ip2   &>/dev/null
+        if [ $?  -ne 0 ];then
+         echo  "ip格式错误，无法通信此ip" 
+         continue
+        else
+         break
+        fi
+    done
+        }
+
+ping3 () {
+    while :   
+    do
+      read -p "请输入第一个节点ip:"  ip3
+      ping  -c 2  -w 2 $ip3   &>/dev/null
+        if [ $?  -ne  0 ];then
+         echo  "ip格式错误，无法通信此ip" 
+         continue
+        else
+         break
+        fi
+    done
+        }
+
+ping4 () {
+    while :   
+    do
+      read -p "请输入第二个节点ip:"  ip4
+      ping  -c 2  -w 2 $ip4   &>/dev/null
+        if [ $? -ne 0  ];then
+         echo  "ip格式错误，无法通信此ip" 
+         continue
+        else
+         break
+        fi
+    done
+        }
+
+
+more_install () {
+ping2
+ping3
+ping4
+cp $data/install.config.3ip.sample  $data/install.config
+sed -i "2s/$num.1/$ip2/1"  $data/install.config
+sed -i "3s/$num.2/$ip3/1"  $data/install.config
+sed -i "4s/$num.3/$ip4/1"  $data/install.config
+sed -i "s/bk.com/$wname/"  $data/globals.env 
+sed -ri "s/[a-z]{6}.[A-Z]{4}.[A-Z]{5}.[A-Z]{4}..[a-z]{1,}[0-9]{1,}+[A-Z]{1,}.*/export PAAS_ADMIN_PASS='123456'/"   $data/globals.env
+                 }
+
+case $version in
+ 1)
+  one_install ;;
+
+ 2)
+  more_install
+   echo '》》》开始进行安装《《《'
+ /data/install/bk_install paas && /data/install/bk_install cmdb && /data/install/bk_install job && /data/install/bk_install app_mgr && /data/install/bk_install bkdata && /data/install/bk_install fta && /data/install/bkcec install gse_agent && /data/install/bkcec install saas-o
+esac
+
---
+++ qiye/pre.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+if [  ! -z $http_proxy  ];then
+ echo  '$http_proxy'为非空，值为"$http_proxy"
+ exit
+fi
+
+if [ ! -z $https_proxy ];then 
+ echo '$https_proxy'为非空，值为"$https_proxy"
+ exit
+fi
+
+systemctl stop NetworkManager       &>/dev/null
+systemctl disable NetworkManager    &>/dev/null
+
+chmod +x /etc/resolv.conf 
+chattr -i /etc/resolv.conf 
+
+systemctl stop firewalld
+systemctl disable firewalld
+
+setenforce 0  &>/dev/null
+sed -i 's/^SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+
+ 
+
+echo "* soft nofile 102400
+* hard nofile 102400"  >>  /etc/security/limits.conf
+
+echo  'ulimit -Hn 102400
+ulimit -Sn 102400' >> /etc/profile
+
+wget 10.0.5.138/bkee_common-2.0.3.11.tgz
+wget 10.0.5.138/bkee_product-2.5.0.14-pretest.tgz
+wget 10.0.5.138/cert_ee-1.2.4_pretest.tgz
+wget 10.0.5.138/install_ee-v1.9.0.tgz
+wget 10.0.5.138/src_2.5.tgz
+
+
+if   [ ! -d /data ];then 
+mkdir /data
+fi 
+echo "开始解压，需要一定时间，请稍等..."
+tar xf /root/bk/qiye/bkee_common-2.0.3.11.tgz   -C /data  && tar xf /root/bk/qiye/bkee_product-2.5.0.14-pretest.tgz  -C /data  && tar xf /root/bk/qiye/cert_ee-1.2.4_pretest.tgz  -C /data/src/cert && tar xf /root/bk/qiye/install_ee-v1.9.0.tgz  -C /data &&  tar xf /root/bk/qiye/src_2.5.tgz  -C /data  && cd /data/src/job/job/ && zip -u0 job-exec.war WEB-INF/lib/*.jar
+if [ $? -ne 0 ];then 
+echo "没有找到软件包，或者软件包名字不对"
+exit
+fi
+
+echo "正在配置yum源,请稍等..."
+mkdir /etc/yum.repos.d/all                &>/dev/null
+mv /etc/yum.repos.d/* /etc/yum.repos.d/all/   &>/dev/null
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.cloud.tencent.com/repo/centos7_base.repo                         
+wget -O /etc/yum.repos.d/epel.repo http://mirrors.cloud.tencent.com/repo/epel-7.repo                                        
+yum clean all                           
+yum repolist                          
+ if [ $?  -ne 0 ];then
+  echo "初始化失败"
+  exit
+ fi
+
+which rsync   >/dev/null
+if [ $? -ne 0 ];then
+ yum -y install rsync
+fi
+
+
+echo "蓝鲸准备已经完成，请执行source /etc/profile 或者重启后进行安装！"
